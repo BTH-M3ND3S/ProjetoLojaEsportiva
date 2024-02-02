2a8142cb0f01b88ba12cd71158cd9e7cd080d186
BTH-M3ND3S <mendessilveriomateus@gmail.com>
2024-02-02 17:10:56 -0300

    "instalação do ts"

---
+++ mpsports/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+      'prettier/@typescript-eslint'
+    ],
+    rules: {
+      // Adicione regras personalizadas se necessário
+    }
+  };
+  
