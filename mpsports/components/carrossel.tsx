9e6b5d4691cb715da44a8b9f5d453ed35845fb75
Paulinxxz <frichepaulinho@gmail.com>
2024-02-06 11:27:22 -0300

    Ajustes no Carrossel

--- mpsports/components/carrossel.tsx
+++ mpsports/components/carrossel.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { ChakraProvider, Flex, Button, Image } from "@chakra-ui/react";
 import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
 const images = [
-  "https://cdn.pixabay.com/photo/2017/09/19/08/54/eyes-2764597_640.jpg",
+  "https://scontent-gru2-2.xx.fbcdn.net/v/t39.30808-6/425853169_3612003439047448_3329244649174627748_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeGjEX7M5N-PG8xvTXVVIgNlRx1X12vOIYRHHVfXa84hhOIvSdiBzzIpXJAosrQKYheIqAbryj9uROBI7gaISpID&_nc_ohc=pulnODm98c4AX978OTT&_nc_ht=scontent-gru2-2.xx&oh=00_AfDLQ13LbBSXgdhEIcvwgeM41GuNikciuH0_d_nWXxdE-A&oe=65C63400",
   "https://cdn.pixabay.com/photo/2015/11/10/08/31/banner-1036483_640.jpg",
   "https://cdn.pixabay.com/photo/2016/09/04/20/14/sunset-1645103_640.jpg",
 ];
